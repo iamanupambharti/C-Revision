694bf5fa8a8e1ebca0aa3d94a6d53d8c2fc5402b
Anupam Bharti <anupam2k7bharti@gmail.com>
2026-02-11 10:58:19 +0530

    Different ways to use the library

---
+++ 02_hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+ 
+using std::cout;
+using std::endl;
+
+int main(){
+    cout << "Hi There, How are You ?"<< endl;
+    return 0;
+}
+
+
+// We can use this way also to use the standard library.
