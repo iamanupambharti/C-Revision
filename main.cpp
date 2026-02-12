c74bcc0f33c98328a2a2c5e61c95fd2bd5ab229a
Anupam Bharti <anupam2k7bharti@gmail.com>
2026-02-12 11:21:01 +0530

    Adding About Comments

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"Are You Happy now?1"<<endl;
+    // cout<<"Are You Happy now?2"<<endl;
+    // cout<<"Are You Happy now?3"<<endl;
+
+    return 0;
+}
+// Commenting:
+/* */
+// we should add comments we are using any complex algorithms or logic 
+// It is a good professional practice.
