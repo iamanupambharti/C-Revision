cb0d3a03baa804c07ace21cd09cabb6ad6605627
Anupam Bharti <anupam2k7bharti@gmail.com>
2026-02-13 12:13:16 +0530

    Seeing different types of the datatypes

---
+++ 02_primitives/data.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int teaLeaves=50;
+    float waterTemp=85.5;//Precision of float upto 6-7 digits 
+    double priceOfTea=299.99;//Precision of double is upto 15-16 digits
+    char tea = 'a';
+    bool isTeaReady = 5; // IF we use anything other than 0 in the value of boolean value like a,5,6,etc...it will considered as true i.e. 1 and one more thing if 
+    // you will use any empty / void it will show error 
+    cout<< isTeaReady<<endl;
+
+
+     
+
+
+
+    return 0;
+}
