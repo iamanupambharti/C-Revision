25ebebb0fae6e9a9078d37ae38d12794b5c66e76
Anupam Bharti <anupam2k7bharti@gmail.com>
2026-02-14 08:54:40 +0530

    Adding Casting

---
+++ casting.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+ 
+float teaPrice= 49.99;
+
+int teaQuantity =2;
+double totalPrice = teaPrice* teaQuantity;
+
+int roundedTeaPrice = (int) teaPrice;
+
+cout << totalPrice << endl ;
+ 
+
+
+return 0;
+}
