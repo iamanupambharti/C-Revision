285cebbbf64449775a13779d180571bd724fd920
Anupam Bharti <anupam2k7bharti@gmail.com>
2026-02-14 08:48:17 +0530

    Adding Modifiers and datatypes

---
+++ modifiers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    unsigned smallTeaPack=1200;
+
+    // float largeTeaStorage=100000000;
+    long long largeTeaStorage=100000000;
+
+    short teaSample=25;
+
+
+
+
+
+    cout << smallTeaPack <<endl;
+    cout << largeTeaStorage <<endl;
+    cout << teaSample <<endl;
+
+
+    return 0;
+} 
