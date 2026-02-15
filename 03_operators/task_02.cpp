20c62eb8e0b97fc462b19dfac0338981f35f5ada
Anupam Bharti <anupam2k7bharti@gmail.com>
2026-02-15 12:51:41 +0530

    task_02 completed

---
+++ 03_operators/task_02.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+int teaBags;
+
+cout<<"Enter the number of bags  you have: ";
+cin >> teaBags;
+
+if(teaBags<=10){
+    // teaBags=teaBags+5;
+    teaBags=+5;
+}
+// else{
+//     teaBags=teaBags;
+    
+// }
+cout<< "The number of teaBags = "<<teaBags;
+
+
+return 0;
+}
